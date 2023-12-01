e22a53a424050c210ed3973125de030bc410adba
Akiluna1 <149687798+Akiluna1@users.noreply.github.com>
2023-12-01 14:18:41 +0100

    fix

---
+++ L05/script.js
@@ -0,0 +1,21 @@
+prompt("Du spazierst in der gegend rum")
+prompt("Du siehst eine Katze auf der Straße");
+prompt("Du denkst dir OMGOMG wie toll!<3")
+prompt("Du gehst zur Katze");
+
+let decision = prompt("Streichelst du die Katze sofort oder pspsp du zuerst?");
+
+if(decision == "streicheln"|| decision=="Streicheln"){
+    prompt("Der Katze hat es nicht gefallen das du sie einfach so gestreichelt hast. Sie faucht dich an un rennt weg! (ง︡'-'︠)ง")
+    prompt("Du gehst traurig weiter")
+}
+else if(decision == "pspsp"|| decision=="PSPSP"){
+    prompt("Die Katze findet es toll das du psppspspspsps gemacht hast. Sie streckt sich zu dir hin und du streichelst sie! ≧◠ᴥ◠≦")
+    prompt("Du gehst glücklich weiter")
+}
+else {
+    prompt('Keine korrekte Eingabe entweder streicheln oder pspsp');
+ }
+
+ 
+ 
